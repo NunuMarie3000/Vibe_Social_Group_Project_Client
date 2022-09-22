66b9f8002d6aa2ea7456e294f7faea1345ea8267
Tyler Morgan <clickjaw@outlook.com>
2022-09-22 00:22:45 -0500

    logo

--- client/src/components/Memory.js
+++ client/src/components/Memory.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "../Nav.css"
+import "../DARKMODE.css"
 
 export default function memory({ author, createdAt, image, content }) {
   return (
-    <div className = 'vibeBox'>
       <Card style = {{width: '18rem', height: "24rem", backgroundColor: "white"}}>
-        <Card.Header style = {{backgroundColor: "#F4EBE8", fontSize: "11px"}}>{author}</Card.Header>
+        <Card.Header style = {{backgroundColor: "#ffffff", fontSize: "11px"}}>{author}</Card.Header>
         <Card.Body style = {{
-          backgroundColor: "#F4EBE8"
+          // backgroundColor: "#F4EBE8",
+          backgroundColor: "#ffffff",
+          boxShadow: '0 3rem 8rem 2rem rgba(0, 0, 0, 0.15)'
         }}>
           <Card.Img src={image} style = {{border: "3px white solid"}}/>
-          <Card.Text>{content}</Card.Text>
-          <Card.Text style = {{fontFamily: "Titan One", textAlign: "right"}}>V</Card.Text>
-          {/* <div className = "logoV">V</div> */}
+          <div className = "overflowCard">
+          <Card.Text style = {{marginTop: "10px", fontFamily: "Rubik"}}>{content}</Card.Text>
+          </div>
+          <div className = "cardLogo">V</div>
         </Card.Body>
         <Card.Footer style = {{fontSize: "10px", textAlign: 'right'}}>{createdAt}</Card.Footer>
-
         {/* <button>Comments</button> accordion */}
       </Card>
-    </div>
   );
 }
