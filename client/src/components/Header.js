0ba0411ab9d25d2ec5a937bca5c36a30dc419f31
Tyler Morgan <clickjaw@outlook.com>
2022-09-21 22:45:08 -0500

    some css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,34 +1,39 @@
-import React from 'react'
-import { Route, Routes, BrowserRouter as Router, Link } from "react-router-dom";
-// import HomePage from "./pages/HomePage";
-// import About from "./pages/About"
+// import React from 'react'
+// import { Route, Routes, BrowserRouter as Router, Link } from "react-router-dom";
+// // import HomePage from "./pages/HomePage";
+// import About from "../pages/About"
+// import DarkMode from "./DarkMode"
+// import "../Nav.css"
 
+// // const Header = ({logout}) => {
+// const Header = () => {
+//   return (
+//     <div>
+//       <Router>
+//         <nav>
+//           <ul>
+//             <li>
+//               {/* <Link to="/">Home</Link> */}
+//             </li>
+//             <li>
+//               {/* <Link to="/about">About</Link> */}
+//             </li>
+//             {/* <li>
+//               <DarkMode/>
+//             </li> */}
+//             <li>
+//               {/* <button onClick = {logout}>Logout</button> */}
+//             </li>
+//           </ul>
+//         </nav>
+//         <Routes>
+//           {/* <Route exact path = "/" ></Route> */}
+//           <Route exact path = "/about" element = {<About/>} />
+//           {/* <Route exact path="/home" element={<HomePage />} /> */}
+//         </Routes>
+//       </Router>
+//     </div>
+//   )
+// }
 
-const Header = ({logout}) => {
-  return (
-    <div>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              {/* <button onClick = {logout}>Logout</button> */}
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          {/* <Route exact path = "/" ></Route> */}
-          {/* <Route exact path = "/about" element = {<About/>} /> */}
-          {/* <Route exact path="/home" element={<HomePage />} /> */}
-        </Routes>
-      </Router>
-    </div>
-  )
-}
-
-export default Header
+// export default Header
--- client/src/components/Memory.js
+++ client/src/components/Memory.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import "../Nav.css"
 
 export default function memory({ author, createdAt, image, content }) {
   return (
-    <div>
-      <Card style = {{width: '18rem', height: "24rem"}}>
-        <Card.Header>{author}</Card.Header>
-        <Card.Body>
-          <Card.Img src={image} />
+    <div className = 'vibeBox'>
+      <Card style = {{width: '18rem', height: "24rem", backgroundColor: "white"}}>
+        <Card.Header style = {{backgroundColor: "#F4EBE8", fontSize: "11px"}}>{author}</Card.Header>
+        <Card.Body style = {{
+          backgroundColor: "#F4EBE8"
+        }}>
+          <Card.Img src={image} style = {{border: "3px white solid"}}/>
           <Card.Text>{content}</Card.Text>
+          <Card.Text style = {{fontFamily: "Titan One", textAlign: "right"}}>V</Card.Text>
+          {/* <div className = "logoV">V</div> */}
         </Card.Body>
-        <Card.Footer>{createdAt}</Card.Footer>
+        <Card.Footer style = {{fontSize: "10px", textAlign: 'right'}}>{createdAt}</Card.Footer>
 
         {/* <button>Comments</button> accordion */}
       </Card>
